111e11cb15ae3156d44d4054cdf5c81653f3b085
Bart Massey <bart@cs.pdx.edu>
2014-03-31 13:24:52 -0700

    added license

--- duvis.c
+++ duvis.c
@@ -1,4 +1,10 @@
-/* Copyright © 2014 Bart Massey */
+/*
+ * Copyright © 2014 Bart Massey
+ * [This program is licensed under the "MIT License"]
+ * Please see the file COPYING in the source
+ * distribution of this software for license terms.
+ */
+
 /* ASCII xdu replacement with reasonable performance. */
 
 #include <assert.h>
